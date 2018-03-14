86f0ee3190b5218f3fbd1b237ff8dcdadfffdffe
Victor Johnson <vicjohnson1213@gmail.com>
2018-03-14 01:53:00 -0500

    NextWorld mod is working.

---
+++ NextWorld/NextWorld.cs
@@ -0,0 +1,18 @@
+﻿using SFMF;
+using UnityEngine;
+
+namespace NextWorld
+{
+    public class NextWorld : IMod
+    {
+        private void Update()
+        {
+            var nextPressed = Input.GetKeyDown(KeyCode.Tab) || Input.GetKeyDown(KeyCode.JoystickButton2);
+            if (LocalGameManager.Singleton.playerState == LocalGameManager.PlayerState.Flying && nextPressed)
+            {
+                LocalGameManager.Singleton.ResetPlayer();
+                LevelGenerator.Singleton.GenerateLevel(WorldManager.NewWorld());
+            }
+        }
+    }
+}
