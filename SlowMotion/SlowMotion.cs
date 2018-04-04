4479e8c76ac4c5f66e8dec3d55881231080f2ce9
Victor Johnson <vicjohnson1213@gmail.com>
2018-04-04 15:35:35 -0500

    Fixed physics bug in slow motion mod.

--- SlowMotion/SlowMotion.cs
+++ SlowMotion/SlowMotion.cs
@@ -11,11 +11,15 @@ namespace SlowMotion
             var slow = Input.GetKey(KeyCode.JoystickButton2) || Input.GetKey(KeyCode.LeftShift);
 
             if (slow)
-                Time.timeScale = Math.Max(0.4f, Time.timeScale - 0.025f);
+            {
+                Time.timeScale = Math.Max(0.6f, Time.timeScale - 0.025f);
+                Time.fixedDeltaTime = .02f * .8f;
+            }
             else
+            {
                 Time.timeScale = Math.Min(1f, Time.timeScale + 0.025f);
-
-            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+                Time.fixedDeltaTime = .02f * Time.timeScale;
+            }
         }
     }
 }
