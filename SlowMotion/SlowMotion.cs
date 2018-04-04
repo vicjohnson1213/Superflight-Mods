2e9ba355a932d40220c7f0d026d9c9573f0de53b
Victor Johnson <vicjohnson1213@gmail.com>
2018-04-04 00:53:45 -0500

    Updated the timing of the slow motion.

--- SlowMotion/SlowMotion.cs
+++ SlowMotion/SlowMotion.cs
@@ -6,14 +6,14 @@ namespace SlowMotion
 {
     public class SlowMotion : IMod
     {
-        private void Update()
+        private void FixedUpdate()
         {
             var slow = Input.GetKey(KeyCode.JoystickButton2) || Input.GetKey(KeyCode.LeftShift);
 
             if (slow)
-                Time.timeScale = Math.Max(0.5f, Time.timeScale - 0.005f);
+                Time.timeScale = Math.Max(0.4f, Time.timeScale - 0.025f);
             else
-                Time.timeScale = Math.Min(1f, Time.timeScale + 0.005f);
+                Time.timeScale = Math.Min(1f, Time.timeScale + 0.025f);
 
             Time.fixedDeltaTime = 0.02F * Time.timeScale;
         }
