aa0b33a77e7c90a9eae5bcf70f065ab5e0a568b5
Victor Johnson <vicjohnson1213@gmail.com>
2018-04-04 00:05:30 -0500

    Added the slow motion mod. Closes #15.

---
+++ SlowMotion/SlowMotion.cs
@@ -0,0 +1,21 @@
+﻿using SFMF;
+using System;
+using UnityEngine;
+
+namespace SlowMotion
+{
+    public class SlowMotion : IMod
+    {
+        private void Update()
+        {
+            var slow = Input.GetKey(KeyCode.JoystickButton2) || Input.GetKey(KeyCode.LeftShift);
+
+            if (slow)
+                Time.timeScale = Math.Max(0.5f, Time.timeScale - 0.005f);
+            else
+                Time.timeScale = Math.Min(1f, Time.timeScale + 0.005f);
+
+            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+        }
+    }
+}
